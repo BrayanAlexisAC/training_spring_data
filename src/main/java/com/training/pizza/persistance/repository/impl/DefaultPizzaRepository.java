92ff7af4f03f9ba03eeb6f4687bcb0c214317b8e
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-04-05 22:19:22 -0600

    Deleted imports without used

--- src/main/java/com/training/pizza/persistance/repository/impl/DefaultPizzaRepository.java
+++ src/main/java/com/training/pizza/persistance/repository/impl/DefaultPizzaRepository.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
