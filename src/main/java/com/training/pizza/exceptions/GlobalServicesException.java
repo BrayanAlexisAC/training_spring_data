9d7ae9ae392762f559ce7a3aead584cc4b61aff4
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-04-18 17:15:41 -0600

    Created Method Argument Not Valid Exception for validations in objects DTO

--- src/main/java/com/training/pizza/exceptions/GlobalServicesException.java
+++ src/main/java/com/training/pizza/exceptions/GlobalServicesException.java
@@ -2,18 +2,23 @@ package com.training.pizza.exceptions;
 
 import com.training.pizza.exceptions.object.GeneralException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalServicesException {
 
     @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<GeneralException> exceptionsHandler(ResponseStatusException exception, WebRequest request){
+    public ResponseEntity<GeneralException> responseStatusExceptionHandler(ResponseStatusException exception, WebRequest request){
         GeneralException generalException = new GeneralException();
         // move this at custom mapper
         generalException.setTimeStamp(LocalDateTime.now());
@@ -22,6 +27,29 @@ public class GlobalServicesException {
         generalException.setMessage(exception.getReason());
 
         return new ResponseEntity<>(generalException, exception.getStatusCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GeneralException> argumentNotValidExceptionHandler(MethodArgumentNotValidException argumentException){
+        GeneralException generalException = new GeneralException();
+        // move this at custom mapper
+        generalException.setTimeStamp(LocalDateTime.now());
+        generalException.setStatus(argumentException.getStatusCode().value());
+        generalException.setMessage("Error with fields: ");
+
+        Map<String,String> mapErrors = argumentException.getAllErrors().stream().collect(
+                Collectors.toMap(
+                    errorKey -> {
+                        String fieldName = ((FieldError) errorKey).getField();
+                        generalException.setMessage(generalException.getMessage() + fieldName + ", ");
+                        return fieldName;
+                    },
+                    errorValue -> Optional.ofNullable(errorValue.getDefaultMessage()).orElse("")
+                )
+        );
+
+        generalException.setErrorsDetail(mapErrors);
+        return new ResponseEntity<>(generalException, argumentException.getStatusCode());
     }
 
 }
--- src/main/java/com/training/pizza/exceptions/object/GeneralException.java
+++ src/main/java/com/training/pizza/exceptions/object/GeneralException.java
@@ -1,15 +1,17 @@
 package com.training.pizza.exceptions.object;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class GeneralException {
 
     private int status;
@@ -20,6 +22,6 @@ public class GeneralException {
 
     private String message;
 
-    private List<String> errorsDetail;
+    private Map<String, String> errorsDetail;
 
 }
