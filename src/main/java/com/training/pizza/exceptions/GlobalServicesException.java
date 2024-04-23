d71203849c4e193d0a5441fcd8255a79a8097d27
Brayan Alexis Aguayo Contreras <braiyan@hotmail.com>
2024-04-23 11:46:31 -0600

    Created mapper and decorator for GeneralException

--- src/main/java/com/training/pizza/exceptions/GlobalServicesException.java
+++ src/main/java/com/training/pizza/exceptions/GlobalServicesException.java
@@ -1,55 +1,28 @@
 package com.training.pizza.exceptions;
 
+import com.training.pizza.domain.mappers.GeneralExceptionMapper;
 import com.training.pizza.exceptions.object.GeneralException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalServicesException {
 
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<GeneralException> responseStatusExceptionHandler(ResponseStatusException exception, WebRequest request){
-        GeneralException generalException = new GeneralException();
-        // move this at custom mapper
-        generalException.setTimeStamp(LocalDateTime.now());
-        generalException.setStatus(exception.getStatusCode().value());
-        generalException.setError(exception.getMessage().substring(0, exception.getMessage().indexOf("\"")));
-        generalException.setMessage(exception.getReason());
+    @Autowired
+    GeneralExceptionMapper mapper;
 
-        return new ResponseEntity<>(generalException, exception.getStatusCode());
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<GeneralException> responseStatusExceptionHandler(ResponseStatusException exception){
+        return new ResponseEntity<>(mapper.toGeneralException(exception), exception.getStatusCode());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<GeneralException> argumentNotValidExceptionHandler(MethodArgumentNotValidException argumentException){
-        GeneralException generalException = new GeneralException();
-        // move this at custom mapper
-        generalException.setTimeStamp(LocalDateTime.now());
-        generalException.setStatus(argumentException.getStatusCode().value());
-        generalException.setMessage("Error with fields: ");
-
-        Map<String,String> mapErrors = argumentException.getAllErrors().stream().collect(
-                Collectors.toMap(
-                    errorKey -> {
-                        String fieldName = ((FieldError) errorKey).getField();
-                        generalException.setMessage(generalException.getMessage() + fieldName + ", ");
-                        return fieldName;
-                    },
-                    errorValue -> Optional.ofNullable(errorValue.getDefaultMessage()).orElse("")
-                )
-        );
-
-        generalException.setErrorsDetail(mapErrors);
-        return new ResponseEntity<>(generalException, argumentException.getStatusCode());
+        return new ResponseEntity<>(mapper.toGeneralException(argumentException), argumentException.getStatusCode());
     }
 
 }
